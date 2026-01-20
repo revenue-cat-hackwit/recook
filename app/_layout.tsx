826b870d26dd743cdb8cbaeebe402d3a2328537b
Putra Afiqi <pikiyolputra@gmail.com>
2026-01-20 23:07:31 +0700

    add custom font support and update Tailwind configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import '../global.css';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    'VisbyCF-Regular': require('../assets/fonts/VisbyCF-Regular.otf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   // All files that use Tailwind CSS classes should be included here
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        visby: ['VisbyCF-Regular'],
+      },
+    },
   },
   plugins: [],
 };
